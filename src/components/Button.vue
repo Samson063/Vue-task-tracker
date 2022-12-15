235ef8b9d8a8b5767b76baa131d6127cc8af3967
Samson063 <furthermaths078@gamil.com>
2022-12-15 17:56:27 +0100

    feat: Added the Add Task button

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'headerButton',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('Clicked')
+    },
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+  name: 'mainHeader',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
