075dca0f1050e2691c42fbe98b1775511583e643
Samson063 <furthermaths078@gamil.com>
2022-12-20 23:27:30 +0100

    added the tasks page and delete button

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+  name: 'taskS',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+}
+</script>
