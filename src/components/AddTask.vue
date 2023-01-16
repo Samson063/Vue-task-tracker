f0f14d3cafa1dc4c74f757eaa492abb86f13c28f
Samson063 <furthermaths078@gamil.com>
2023-01-16 10:56:31 +0100

    feat: Added the Add Task page

---
+++ src/components/AddTask.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Day & Time</label>
+      <input type="text" name="day" placeholder="Add Day & Time" />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+}
+</script>
